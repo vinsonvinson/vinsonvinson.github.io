4059077f1fe874a305589d8735874317eaa2c834
SketPlayer <riohu137@gmail.com>
2023-05-26 22:29:30 +0700

    Starting Page

--- uas-pti/src/App.js
+++ uas-pti/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerwrap">
+      <div className="header">
+        <img className="logoIndo"
+             src='https://api2.kemenparekraf.go.id/storage/app/uploads/public/621/437/638/621437638c977337188787.png'
+             alt='WonderfulIndonesia'></img>
+        <img className="logoUMN"
+             src='https://seeklogo.com/images/U/universitas-multimedia-nusantara-umn-logo-48A807B801-seeklogo.com.png'
+             alt='UMN'></img>
+        <img className='logoF4'
+             src='https://seeklogo.com/images/F/Fantastic_Four-logo-95A3DB03CB-seeklogo.com.png'
+             alt='f4'></img>
+             {/*Logo Kelompok jika diklik akan ke menuju About Us (jika ada)*/}
+      </div>
+      <br />
+      <div className="inputName">
+        <input type='name' placeholder='INPUT NAME'></input>
+      </div>
+      <br />
+      <div className='start'>
+        <button className='startBtn'>START</button>
+      </div>
     </div>
   );
 }
